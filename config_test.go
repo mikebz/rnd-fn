1b3a460e860988fc51b6ac2d616480f9f518b918
Mike Borozdin <mikebz@google.com>
2020-12-23 15:37:47 -0800

    added the test for multiple field spec=s

--- config_test.go
+++ config_test.go
@@ -19,3 +19,28 @@ func TestSimpleConfig(t *testing.T) {
 	assert.NotNil(t, tr.FieldSpecs)
 	assert.Len(t, tr.FieldSpecs, 1)
 }
+
+// test that verifies that multipel field specs
+// are parsed in to multiple FieldSpec objects
+func TestMultipleSpecs(t *testing.T) {
+
+	config := `---
+fieldprefix: rnd
+fieldSpecs:
+  - path: metadata/namespace
+    group: foo
+    kind: Bar
+    apiVersion: foo/v1beta1
+    create: true
+`
+
+	resmapFactory := newResMapFactory()
+	pluginHelpers := newPluginHelpers(resmapFactory)
+
+	tr := transformer{}
+	err := tr.Config(pluginHelpers, []byte(config))
+	assert.NoError(t, err)
+	assert.NotNil(t, tr.FieldPrefix)
+	assert.NotNil(t, tr.FieldSpecs)
+	assert.Len(t, tr.FieldSpecs, 1)
+}
